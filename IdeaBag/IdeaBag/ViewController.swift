90aaca65bc664938dcb9711a2205148253758d12
Adriano Soares <gsoares.adriano@gmail.com>
2017-02-18 21:42:52 -0200

    Initial commit

---
+++ IdeaBag/IdeaBag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdeaBag
+//
+//  Created by Adriano Soares on 18/02/17.
+//  Copyright © 2017 Adriano Soares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
